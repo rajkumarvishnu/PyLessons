7ce8489e019bf3f8e226b40666ea7ebe9deb9f61
rajkumarvishnu <vishnu.rajkumar@live.com>
2013-11-04 15:51:21 +0530

    Until 33

---
+++ ex29.py
@@ -0,0 +1,29 @@
+people =20
+cats = 30
+dogs = 15
+
+if people <cats:
+	print "Too many cats"
+	
+if people > cats:
+	print "Not many cats"
+	
+if people <dogs:
+	print "the world is drooled on"
+
+if people > dogs:
+	print " the world is drooled on"
+
+if people > dogs:
+		print "the world is dry"
+		
+dogs+=5
+
+if people >=dogs:
+	print "people are greater than equal to dogs"
+	
+if people <=dogs:
+	print "people are less than equal to dogs"
+	
+if people == dogs:
+	print "people are dogs"
---
+++ ex30.py
@@ -0,0 +1,30 @@
+people = 40
+cars = 40
+busses = 40
+
+if cars > people:
+	print "we should take the cars"
+
+	
+elif cars < people:
+	print "we should not take the cars"
+
+else:
+	print "we cant decide"
+	
+if busses > cars:
+	print "That's too many busses"	
+
+elif busses<cars:
+	print "may be we could take the busses"
+	
+else:
+	print "we still can't decide"
+	
+if people > busses:
+	print "Alright, lets just take teh busses"
+
+else:
+	print "Fine, let's stay home then"
+	
+	
